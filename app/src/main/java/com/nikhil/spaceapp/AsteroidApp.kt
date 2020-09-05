7151080291175f3656c3763f21b94c63981acbce
Nikhil <nikhilmht33@gmail.com>
2020-09-05 19:47:40 +0530

    Add Timber Debug and Release Tree

--- app/src/main/java/com/nikhil/spaceapp/AsteroidApp.kt
+++ app/src/main/java/com/nikhil/spaceapp/AsteroidApp.kt
@@ -1,7 +1,27 @@
 package com.nikhil.spaceapp
 
 import android.app.Application
+import com.nikhil.spaceapp.util.ReleaseTree
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class AsteroidApp : Application()
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                override fun createStackElementTag(element: StackTraceElement): String? {
+                    return String.format(
+                        "Class:%s: Line: %s, Method: %s",
+                        super.createStackElementTag(element),
+                        element.lineNumber,
+                        element.methodName
+                    )
+                }
+            })
+        } else {
+            Timber.plant(ReleaseTree())
+        }
+    }
+}
