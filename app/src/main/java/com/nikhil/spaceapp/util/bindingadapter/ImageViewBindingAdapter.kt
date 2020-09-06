ed3d313dfc34d2568d0a7e61bb7bbbab31959578
Nikhil <nikhilmht33@gmail.com>
2020-09-06 14:38:01 +0530

    Replace NASA API image url with Unsplash

--- app/src/main/java/com/nikhil/spaceapp/util/bindingadapter/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/nikhil/spaceapp/util/bindingadapter/ImageViewBindingAdapter.kt
@@ -6,6 +6,9 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import timber.log.Timber
 
+const val URL_UNSPLASH =
+    "https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+
 @BindingAdapter("imageUrl")
 fun loadImage(imageView: ImageView, url: String?) {
     if (url != null) {
@@ -13,8 +16,10 @@ fun loadImage(imageView: ImageView, url: String?) {
         Timber.d("original image url: $url")
         Timber.d("trimmed image url: ${url.trim()}")
 
+        //There seems to be some issue with NASA API URL format
         Glide.with(imageView.context)
-            .load(url)
+//            .load(url)
+            .load(URL_UNSPLASH)
             .apply(requestOption)
             .into(imageView)
     }
