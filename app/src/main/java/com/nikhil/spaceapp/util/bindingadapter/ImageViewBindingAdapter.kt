af6c339b660f4f10a98e15d76f4646a14fc116f3
Nikhil <nikhilmht33@gmail.com>
2020-09-06 15:14:17 +0530

    Verified that issue exists with API image url

--- app/src/main/java/com/nikhil/spaceapp/util/bindingadapter/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/nikhil/spaceapp/util/bindingadapter/ImageViewBindingAdapter.kt
@@ -13,9 +13,17 @@ fun loadImage(imageView: ImageView, url: String?) {
         Timber.d("original image url: $url")
         Timber.d("trimmed image url: ${url.trim()}")
 
-        //There seems to be some issue with NASA API URL format
+        /**
+         * Verified that there is some issue with NASA API URL format that we are getting from API
+         * as APOD(Astronomy Picture of the Day) API response URL:
+         *      https://apod.nasa.gov/apod/image/2009/CrabNebula_Hubble_960.jpg
+         * is working fine.
+         */
+        //
         Glide.with(imageView.context)
             .load(url)
+//            .load("https://ssd.jpl.nasa.gov/sbdb.cgi")
+//            .load("https://apod.nasa.gov/apod/image/2009/CrabNebula_Hubble_960.jpg")
             .apply(requestOption)
             .into(imageView)
     }
